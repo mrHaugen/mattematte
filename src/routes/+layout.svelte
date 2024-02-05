e0dea04771af2eef4ce296f4a9a73abd9fd15188
Brage Haugen <brage.haugen@gmail.com>
2024-02-05 13:48:38 +0100

    Add logging with plausible

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.pcss';
 </script>
 
+<svelte:head>
+	<script defer data-domain="mattematte.no" src="https://plausible.io/js/script.js"></script>
+</svelte:head>
+
 <div class="flex h-screen items-center justify-center">
 	<slot />
 </div>
