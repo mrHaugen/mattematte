13e4c5d8365075d4b5c5ec1ce1a3c50ec3743948
Brage Haugen <brage.haugen@gmail.com>
2024-02-18 22:38:08 +0100

    Add version 2

---
+++ src/routes/v2/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { Questions } from './utils';
+	import { onMount } from 'svelte';
+
+	let selectedTables = [2, 3, 4, 5, 6, 7, 8, 9];
+
+	let operation = 'multiplication';
+	const multiplicationTable = new Questions(operation);
+
+	let task = $state(undefined);
+	let taskStartTime: number;
+
+	function getNewTask() {
+		task = multiplicationTable.getRandomTask(selectedTables);
+		taskStartTime = Date.now();
+	}
+
+	function checkAnswer(answer) {
+		const thinkTime = Date.now() - taskStartTime;
+		const answerIsCorrect = multiplicationTable.checkAnswer(task, answer, thinkTime);
+		if (answerIsCorrect) {
+			getNewTask();
+		}
+	}
+
+	onMount(() => {
+		getNewTask();
+	});
+</script>
+
+<div class="flex flex-col">
+	<div>Questions</div>
+	{#if task}
+		<div>
+			{task.question.A}
+			{#if operation === 'multiplication'}
+				·
+			{:else if operation === 'division'}
+				:
+			{/if}
+			{task.question.B}
+		</div>
+		<div class="flex flex-row">
+			{#each task.alternatives as alternative, index}
+				<button class="p-4 border-2 rounded-md w-16 h-16" onclick={() => checkAnswer(alternative)}
+					>{alternative}</button
+				>
+			{/each}
+		</div>
+	{/if}
+</div>
