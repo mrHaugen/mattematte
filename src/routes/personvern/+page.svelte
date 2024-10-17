bc0d0bde8ad0a998c7e24f523239acaba603aab5
Brage Haugen <brage.haugen@gmail.com>
2024-10-17 22:21:17 +0200

    Add privacy note

---
+++ src/routes/personvern/+page.svelte
@@ -0,0 +1,17 @@
+<div class="space-y-4">
+    <h1 class="text-xl">
+        Personvernerklæring for mattematte.no
+    </h1>
+    <p>
+        Vi benytter plausible.io for å hente inn <span class="font-bold"> anonymisert aggregert statistikk </span> om bruk av nettsiden.
+    </p>
+    <p>
+        mattematte.no samler ikke inn noen opplysninger som kan identifisere deg som bruker.
+    </p>
+    <p>
+        mattematte.no er utviklet av Brage Haugen.
+    </p>
+    <p class="pt-2">
+        Har du spørmål kan du kontakte meg på post@1618.no eller +47 924 90 968.
+    </p>
+</div>
